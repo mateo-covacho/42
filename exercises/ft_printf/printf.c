8f7d157a678b24697b532be60bc82dbf6ab2b10b
mateo-covacho <mateocovachov2@gmail.com>
2024-08-31 18:05:39 +0200

    asd

--- exercises/ft_printf/printf.c
+++ exercises/ft_printf/printf.c
@@ -16,6 +16,80 @@ int	ft_printf(const char *format, ...)
 {
 	va_list args;
 	va_start(args, format);
+	if (!format)
+		return 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				ft_putstr(str);
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				ft_putchar(c);
+			}
+			else if (*format == 'd')
+			{
+				int num = va_arg(args, int);
+				ft_putnbr(num);
+			}
+			else if (*format == 'i')
+			{
+				int num = va_arg(args, int);
+				ft_putnbr(num);
+			}
+			else if (*format == 'u')
+			{
+				int num = va_arg(args, int);
+				ft_putnbr(num);
+			}
+			else if (*format == 'p')
+			{
+				void *ptr = va_arg(args, void *);
+				if (!ptr)
+					ft_putstr("(nil)");
+				else {
+					ft_putstr("0x");
+					ft_puthex((unsigned long)ptr);
+				}
+			}
+			else if (*format == 'x')
+			{
+				int num = va_arg(args, int);
+				ft_puthex(num);
+			}
+			else if (*format == 'X')
+			{
+				int num = va_arg(args, int);
+				ft_put_upper_hex(num);
+			}
+			else if (*format == '%')
+			{
+				ft_putchar('%');
+			}
+		}
+		else
+			ft_putchar(*format);
+		format++;
+	}
+	va_end(args);
+	return 0;
+}
+
+
+
+int	printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	/* if (!format) */
+	/* 	return 0; */
 
 	while (*format)
 	{
